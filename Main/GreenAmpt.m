690f1ba303a55bbb2cdec3e684b1e86380061bb2
Alice Giorgetti <alice.giorgetti@epfl.ch>
2025-06-06 12:06:07 +0200

    Update GreenAmpt.m  with comments

--- Main/GreenAmpt.m
+++ Main/GreenAmpt.m
@@ -1,5 +1,6 @@
 function [GreenAmpt_output] = GreenAmpt(station, timescale, K_s, Psi, theta_i, theta_s)
 
+    % Moisture deficit [-]
     theta_d = theta_s - theta_i;
 
     % Load appropriate dataset based on station
@@ -18,22 +19,27 @@ function [GreenAmpt_output] = GreenAmpt(station, timescale, K_s, Psi, theta_i, t
 
     % Handle continuous full year case
     if timescale == "Year 2024"
-        t = hours(T.time - T.time(1));
+        t = hours(T.time - T.time(1)); % Convert time to hours relative to the start
         precip = T.rre150h0 / 10;  % mm to cm
 
-        F_sol = zeros(size(t));
-        f_star = zeros(size(t));
-        f_sol = zeros(size(t));
-        z = zeros(size(t));
+        % Initialize vectors to store results
+        F_sol = zeros(size(t)); % Cumulative infiltration [cm]
+        f_star = zeros(size(t)); % Potential infiltration rate [cm/h]
+        f_sol = zeros(size(t)); % Actual infiltration rate [cm/h]
+        z = zeros(size(t)); % Wetting front depth [cm]
 
+        % Time loop to compute infiltration at each time step
         for i = 1:length(t)
-            F = K_s * t(i);
-            tol = 1e-6;
-            max_iter = 100;
-            iter = 0;
+            F = K_s * t(i);  % Initial guess for cumulative infiltration
+            tol = 1e-6; % Tolerance for convergence
+            max_iter = 100; % Max number of iteration allowed
+            iter = 0; % Counter 
+
+             % Define Green-Ampt cumulative infiltration function and its derivative
             F_fun = @(F) F - Psi * theta_d * log(1 + F / (Psi * theta_d)) - K_s * t(i);
             F_prime = @(F) 1 - (Psi * theta_d) / (F + Psi * theta_d);
 
+            % Newton-Raphson iteration to solve for cumulative infiltration F
             while iter < max_iter
                 f_val = F_fun(F);
                 f_der = F_prime(F);
@@ -46,11 +52,12 @@ function [GreenAmpt_output] = GreenAmpt(station, timescale, K_s, Psi, theta_i, t
             end
 
             F_sol(i) = F;
-            f_star(i) = K_s * (1 + (Psi * theta_d) / F);
-            f_sol(i) = min(f_star(i), precip(i));
-            z(i) = F / theta_d;
+            f_star(i) = K_s * (1 + (Psi * theta_d) / F); % Compute potential infiltration rate
+            f_sol(i) = min(f_star(i), precip(i)); % Determine actual infiltration rate based on precipitation
+            z(i) = F / theta_d; % Compute wetting front depth
         end
 
+        % Store results
         GreenAmpt_output = table(t, f_sol, F_sol, f_star, z, ...
             'VariableNames', {'t[h]', 'f(t)', 'F(t)', 'f*(t)', 'z(t)'});
 
